503ed0304cd649441807c2be15039616943b64e4
Orloh <ohercelli@protonmail.com>
2025-10-03 00:25:28 +0200

    ft_memset.c

---
+++ ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orhernan <ohercelli@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 00:03:40 by orhernan          #+#    #+#             */
+/*   Updated: 2025/10/03 00:24:06 by orhernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memset(void *p, int c, size_t len)
+{
+	size_t	i;
+
+	if (!p)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		*(unsinged char*)(p + i) = (unsigned char)c;
+		i++;
+	}
+	return (p);
+}
