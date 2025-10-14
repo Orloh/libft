673c552d271e8af861600a51b071593e1b6422b8
Orloh <ohercelli@protonmail.com>
2025-10-14 23:40:42 +0200

    update

--- ft_memchr.c
+++ ft_memchr.c
@@ -16,10 +16,10 @@ void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	uc;
 	unsigned char	*ucs;
-	
+
 	if (s == NULL)
 		return (NULL);
-	uc = (unsigned char) c; 
+	uc = (unsigned char) c;
 	ucs = (unsigned char *)s;
 	while (n > 0)
 	{
