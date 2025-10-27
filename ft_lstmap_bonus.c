d7200a142c79db30ab16bcdce1873952378f71fd
orhernan <orhernan@student.42madrid.com@example.com>
2025-10-27 16:04:29 +0100

    update

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -16,15 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_elm;
+	void	*new_content;
 
 	if (!lst || !f || !del)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		new_elm = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_elm = ft_lstnew(new_content);
 		if (!new_elm)
 		{
+			if (new_content)
+				del(new_content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
